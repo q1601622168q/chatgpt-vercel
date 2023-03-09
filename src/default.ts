0e7d6241ff909c2012a3c2bbba648ea2dcf209d1
wubolive <31678260+wubolive@users.noreply.github.com>
2023-03-09 15:18:13 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,8 +7,5 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可使用。
-- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
-- 由 [ourongxing](https://github.com/ourongxing) 修改自 [Diu](https://github.com/ddiu8081/chatgpt-demo)，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
-- 如果本项目对你有所帮助，可以给小猫[买点零食](https://cdn.jsdelivr.net/gh/ourongxing/chatgpt-vercel/assets/reward.gif)，但不接受任何付费功能请求。
+- 输入您的问题，与OpenAI进行对话吧。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
